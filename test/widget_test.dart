0ec29086d0cb2ee638f7a9999c38323d56249618
A-X-Z-Y-T-E <b23296@students.iitmandi.ac.in>
2025-02-06 19:52:04 +0530

    added Dependencies and modified git ignore to remove ios, android and assets

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dp_project/main.dart';
+import 'package:Vital_Monitor/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
